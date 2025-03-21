4f7bc8a2e09946a873e3ebc6596d05badaf5dee8
Harish10-Stack <harishkaladharan10@gmail.com>
2025-03-21 11:38:39 +0530

    Add files via upload

---
+++ Task[19.03].js
@@ -0,0 +1,22 @@
+//  Use the toFixed() method to round the number 7.45678 to two decimal places.
+let num = 7.45678;
+let TotalNum = num.toFixed(2);
+
+console.log(TotalNum);
+
+// Use the toPrecision() method to format the number 123.456789 to a precision of four digits.
+const f = 123.456789;
+console.log(f.toPrecision(4));
+
+// Use Object.is(obj1, obj2) to check if two objects are the same.
+const a1 = 10;
+const a2 = 10;
+
+console.log(Object.is(a1, a2));
+
+
+
+
+
+
+
